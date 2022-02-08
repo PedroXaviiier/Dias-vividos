76f821a4172febfe4fb15a8f800b420ba76c8442
PedroXaviiier <phxavier.phx@gmail.com>
2022-02-08 14:05:22 -0300

    Add project files.

---
+++ Dias vividos/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Escreva um algoritmo para ler o nome e a idade de uma pessoa, e exibir
+quantos dias de vidaela possui. Considere sempre anos completos, e que um
+ano possui 365 dias. Ex: uma pessoa com 19 anos possui 6935 dias de vida; veja
+um exemplo de saída: MARIA, VOCÊ JÁ VIVEU 6935 DIAS.
+*/
+
+
+
+
+
+using System;
+
+namespace Dias_vividos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe sua idade: ");
+            Console.Write("Informe seu nome: ");
+            string nome = Console.ReadLine();
+            int idade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{nome}, você viveu {idade * 365} dias");
+        }
+    }
+}
